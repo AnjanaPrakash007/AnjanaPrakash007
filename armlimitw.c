60ba60400c920f60f29d6540cf8946a3f303e70e
Anjana Prakash <82230278+AnjanaPrakash007@users.noreply.github.com>
2021-05-19 12:34:31 +0530

    Add files via upload

---
+++ armlimitw.c
@@ -0,0 +1,30 @@
+/*When the sum of the cube of the individual digits of a number
+ is equal to that number, the number is called Armstrong number. For example 153.
+Sum of its divisor is 13 + 53;+ 33; = 1+125+27 = 153*/
+#include <stdio.h>
+
+void main(){
+    int num,r,sum,temp;
+    int a,z;
+
+    printf("enter starting number of range: ");
+    scanf("%d",&a);
+
+    printf("enter ending number of range : ");
+    scanf("%d",&z);
+
+    printf("Armstrong numbers in given range are: ");
+    for(num=a;num<=z;num++){
+         temp=num;
+         sum = 0;
+
+         while(temp!=0){
+             r=temp % 10;
+             temp=temp/10;
+             sum=sum+(r*r*r);
+         }
+         if(sum==num)
+             printf("%d ",num);
+    }
+printf("\n");
+}
