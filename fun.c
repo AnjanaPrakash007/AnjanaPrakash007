8c27f80c7643fed6b93ecd8d4702a756fc2a47ed
Anjana Prakash <82230278+AnjanaPrakash007@users.noreply.github.com>
2021-06-05 12:40:53 +0530

    Add files via upload

---
+++ fun.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+//function prototype declaration
+void message();
+int main()
+{
+  message();//function call
+  printf("welcome to C\n");
+  printf("**************");
+  return(0);
+
+}
+void message()//function definition
+{
+  printf("************\n");
+  printf("hello Anjana Prakash\n");
+}
